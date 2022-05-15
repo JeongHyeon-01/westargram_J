d26ab0864334216f3a70adbc78749901933372cd
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-05-15 18:27:34 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+   	'default' : {
+    	'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'westagram_serial',
+        'USER': 'root',
+        'PASSWORD': 'ghkdwjdgus12!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-l33912*hqtako52=deyr6t)w-sba-p_rw%mkvo3q8j5e)_fa3n'
